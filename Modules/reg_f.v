372b22b1eefb5052bf51007d41565c0126632437
orszoooo <pawel.orszulik7@gmail.com>
2024-01-06 18:53:35 +0100

    Reg_f minor fixes and schematic update

--- Modules/reg_f.v
+++ Modules/reg_f.v
@@ -4,39 +4,46 @@ module reg_f #(
     parameter WIDTH = 8,
     parameter SIZE = 9
 )(
-    CLK,
-    IN,
-    EN,
-    SEL,
-    PORT,
-    OUT
+    clk,
+    in,
+    en,
+    sel,
+    port,
+    out
 );
 
-input [WIDTH-1:0] IN;
-inout [WIDTH-1:0] PORT;
-input EN, CLK;
-input [$clog2(SIZE)-1:0] SEL;
-output [WIDTH-1:0] OUT;
+input [WIDTH-1:0] in;
+inout [WIDTH-1:0] port;
+input en, clk;
+input [$clog2(SIZE)-1:0] sel;
+output [WIDTH-1:0] out;
 
 reg [WIDTH-1:0] REG_FILE [SIZE:0];
 reg PORT_EN = 1'b1;
+integer i = 0;
 
-assign PORT = (PORT_EN) ? IN : {WIDTH{1'bz}};
-
-always@(posedge CLK) begin
-    if(EN) begin
-        if(SEL == {$clog2(SIZE){1'b1}}) begin //To write to PORT SEL == 4'b1111 {$clog2(SIZE){1'b1}
-            PORT_EN <= 1'b1;
-            REG_FILE[SIZE-1] <= IN;
-        end
-        REG_FILE[SEL] <= IN;
-    end 
-    else begin
-        PORT_EN <= 1'b0;
-        REG_FILE[SIZE-1] <= PORT; //To read PORT SEL == 4'b1000 - nineth reg
+initial begin
+    for(i=0;i<SIZE;i++) begin
+        REG_FILE[i] = {WIDTH{1'b0}};
     end
 end
 
-assign OUT = REG_FILE[SEL];
+assign port = (PORT_EN) ? in : {WIDTH{1'bz}};
+
+always@(posedge clk) begin
+    if(en) begin
+        if(sel == {$clog2(SIZE){1'b1}}) begin //To write to port sel == 4'b1111 {$clog2(SIZE){1'b1}
+            PORT_EN <= 1'b1;
+            REG_FILE[SIZE-1] <= in;
+        end
+        REG_FILE[sel] <= in;
+    end 
+    else begin
+        PORT_EN <= 1'b0;
+        REG_FILE[SIZE-1] <= port; //To read port sel == 4'b1000 - nineth reg
+    end
+end
+
+assign out = REG_FILE[sel];
 
 endmodule 
--- Modules/tb/reg_f_tb.v
+++ Modules/tb/reg_f_tb.v
@@ -3,27 +3,27 @@
 module reg_f_tb;
 
 reg CLK;
-parameter W = 8;
-parameter S = 9;
-reg [W-1:0] IN;
+parameter WIDTH = 8;
+parameter SIZE = 9;
+reg [WIDTH-1:0] IN;
 reg EN;
-reg [$clog2(S)-1:0] SEL;
-wire [W-1:0] PORT;
-reg [W-1:0] PORT_REG;
-wire [W-1:0] PORT_RECV;
-wire [W-1:0] OUT;
+reg [$clog2(SIZE)-1:0] SEL;
+wire [WIDTH-1:0] PORT;
+reg [WIDTH-1:0] PORT_REG;
+wire [WIDTH-1:0] PORT_RECV;
+wire [WIDTH-1:0] OUT;
 integer i;
 
 reg_f #(
-    .WIDTH(W),
-    .SIZE(S)
+    .WIDTH(WIDTH),
+    .SIZE(SIZE)
 ) UUT (
-    .CLK(CLK),
-    .IN(IN),
-    .EN(EN),
-    .SEL(SEL),
-    .PORT(PORT),
-    .OUT(OUT)
+    .clk(CLK),
+    .in(IN),
+    .en(EN),
+    .sel(SEL),
+    .port(PORT),
+    .out(OUT)
 );
 
 assign PORT = PORT_REG;
@@ -74,7 +74,7 @@ end
 
 // Writing VCD waveform
 initial begin
-	$dumpfile("cpu_sim.vcd");
+	$dumpfile("./Output/reg_f_sim.vcd");
 	$dumpvars(0, UUT);
 	$dumpon;
 end
